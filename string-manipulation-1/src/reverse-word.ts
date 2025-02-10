2757eb97625138625efd7f1114719016444dff2b
Frank Kang <holycow001@gmail.com>
2025-02-10 22:00:36 +0000

    completed assignment

--- string-manipulation-1/src/reverse-word.ts
+++ string-manipulation-1/src/reverse-word.ts
@@ -1,1 +1,8 @@
 /* exported reverseWord */
+function reverseWord(word: string): string {
+  let reversedArray: string = '';
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversedArray += word[i];
+  }
+  return reversedArray;
+}
