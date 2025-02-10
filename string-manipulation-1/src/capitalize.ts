a1c84e1cec5bb0d244c1d1809f320860f5a8619d
Frank Kang <holycow001@gmail.com>
2025-02-10 18:58:55 +0000

    inprogress

--- string-manipulation-1/src/capitalize.ts
+++ string-manipulation-1/src/capitalize.ts
@@ -1,12 +1,1 @@
 /* exported capitalize */
-function capitalize(str: string): string {
-  let newStr: string = '';
-  for (let i = 0; i < str.length - 1; i++) {
-    if (i === 0) {
-      newStr += str[i].toUpperCase;
-    } else {
-      newStr += str[i].toLowerCase;
-    }
-  }
-  return newStr;
-}
