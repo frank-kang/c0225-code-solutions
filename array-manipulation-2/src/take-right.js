94f4f07c88c39c624d6f2f70e55bdd1ba6fbca18
Frank Kang <holycow001@gmail.com>
2025-02-11 22:00:05 +0000

    in progress at dropRight

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,2 +1,15 @@
 'use strict';
 /* exported takeRight */
+function takeRight(array, count) {
+  const newArray = [];
+  if (array.length === 0) {
+    return [];
+  } else if (count > array.length) {
+    return array;
+  } else {
+    for (let i = array.length - count; i < array.length; i++) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
--- array-manipulation-2/src/take-right.ts
+++ array-manipulation-2/src/take-right.ts
@@ -1,1 +1,14 @@
 /* exported takeRight */
+function takeRight(array: any[], count: number): any[] {
+  const newArray: any[] = [];
+  if (array.length === 0) {
+    return [];
+  } else if (count > array.length) {
+    return array;
+  } else {
+    for (let i = array.length - count; i < array.length; i++) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
