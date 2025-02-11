445939bd22b42c02b7192dac1caf82180c9a0743
Frank Kang <holycow001@gmail.com>
2025-02-11 22:51:47 +0000

    working on dropRight function

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,2 +1,13 @@
 'use strict';
 /* exported dropRight */
+function dropRight(array, count) {
+  const newArray = [];
+  if (count >= array.length) {
+    return [];
+  } else {
+    for (let i = 0; i < count; i++) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
--- array-manipulation-2/src/drop-right.ts
+++ array-manipulation-2/src/drop-right.ts
@@ -1,1 +1,12 @@
 /* exported dropRight */
+function dropRight(array: any[], count: number): any[] {
+  const newArray: any[] = [];
+  if (count >= array.length) {
+    return [];
+  } else {
+    for (let i = 0; i < count; i++) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
