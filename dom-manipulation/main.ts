64f119b6c910e9beaf6bcc3ac00316368d35f8d7
Frank Kang <holycow001@gmail.com>
2025-02-18 18:09:51 +0000

    stored clicks in variable not dom and made if statements simpler used template literal not split

--- dom-manipulation/main.ts
+++ dom-manipulation/main.ts
@@ -1,29 +1,35 @@
 const $classHotButton = document.querySelector('.hot-button');
 const $classClickCount = document.querySelector('.click-count');
+let count: number = 0;
 if (!$classHotButton) throw new Error('$classHotButton was not found');
 
 $classHotButton.addEventListener('click', () => {
   if (!$classHotButton || !$classClickCount) {
     throw new Error('The $hotButton or $clickCount query failed');
   }
-  const str: any = $classClickCount?.textContent;
+  /* const str: any = $classClickCount?.textContent;
   const parts: any[] = str.split(' ');
   let updatedStr: any = '';
   let count = parseInt(parts[1]);
+  updatedStr = parts[0] + ' ' + count; */
   count++;
-  updatedStr = parts[0] + ' ' + count;
-  $classClickCount.textContent = updatedStr;
+  let temp: string = '';
+  // $classClickCount.textContent = updatedStr;
   if (count < 4) {
-    $classHotButton.className = 'hot-button cold';
-  } else if (count >= 4 && count < 7) {
-    $classHotButton.className = 'hot-button cool';
-  } else if (count >= 7 && count < 10) {
-    $classHotButton.className = 'hot-button tepid';
-  } else if (count >= 10 && count < 13) {
-    $classHotButton.className = 'hot-button warm';
-  } else if (count >= 13 && count < 16) {
-    $classHotButton.className = 'hot-button hot';
+    // $classHotButton.className = 'hot-button cold';
+    temp = 'cold';
+  } else if (count < 7) {
+    temp = 'cool';
+  } else if (count < 10) {
+    temp = 'tepid';
+  } else if (count < 13) {
+    temp = 'warm';
+  } else if (count < 16) {
+    temp = 'hot';
   } else {
-    $classHotButton.className = 'hot-button nuclear';
+    temp = 'nuclear';
   }
+
+  $classHotButton.className = `hot-button ${temp}`;
+  $classClickCount.textContent = `Clicks: ${count}`;
 });
