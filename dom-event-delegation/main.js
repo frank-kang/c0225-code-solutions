e5ed82eed88753a779e6f5d263a319c118faa444
Frank Kang <holycow001@gmail.com>
2025-02-18 23:27:12 +0000

    completed dom-event-delegation exercise

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,11 +1,10 @@
+'use strict';
 const $classTaskList = document.querySelector('.task-list');
-
 if (!$classTaskList) throw new Error('$classTaskList does not exist');
-$classTaskList.addEventListener('click', (event: Event) => {
-  const eventTarget = event.target as HTMLElement;
+$classTaskList.addEventListener('click', (event) => {
+  const eventTarget = event.target;
   console.log(eventTarget);
   console.log(eventTarget.tagName);
-
   if (eventTarget.tagName === 'BUTTON') {
     const et = eventTarget;
     const closest = et.closest('.task-list-item');
