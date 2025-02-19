2a3aa5b8f6d04e0009f1abc09b72411bd3c76f2d
Frank Kang <holycow001@gmail.com>
2025-02-19 17:56:14 +0000

    completed ts-modal exercise

--- typescript-modal/main.js
+++ typescript-modal/main.js
@@ -1,14 +1,12 @@
+'use strict';
 const $classOpenModal = document.querySelector('.open-modal');
 const $classDismissModal = document.querySelector('.dismiss-modal');
 const $tagElement = document.querySelector('dialog');
-
 if (!$classOpenModal || !$classDismissModal || !$tagElement)
   throw new Error('One or more elements do not exist');
-
 $classOpenModal.addEventListener('click', () => {
   $tagElement.showModal();
 });
-
 $classDismissModal.addEventListener('click', () => {
   $tagElement.close();
 });
