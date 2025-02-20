426ff7fd5a9817f324d33ed1557cca4b01ccf9c6
Frank Kang <holycow001@gmail.com>
2025-02-20 17:09:26 +0000

    almost complet ts-typing-tutor

---
+++ typescript-typing-tutor/main.js
@@ -0,0 +1,30 @@
+'use strict';
+function typingTutor() {
+  const $tagSpans = document.querySelectorAll('span');
+  console.log('beginning', $tagSpans);
+  let currentIndex = 0;
+  let $tagSpan = $tagSpans[currentIndex];
+  $tagSpan.classList.add('active');
+  document.addEventListener('keydown', (e) => {
+    console.log('tagSpan:', $tagSpan);
+    if ($tagSpan.textContent === e.key && currentIndex < $tagSpans.length) {
+      $tagSpan.classList.add('green-font');
+      if ($tagSpan.classList.contains('red-font')) {
+        $tagSpan.classList.toggle('red-font');
+      }
+      if ($tagSpan.classList.contains('active')) {
+        $tagSpan.classList.toggle('active');
+      }
+      currentIndex++;
+      if (currentIndex < $tagSpans.length) {
+        $tagSpan = $tagSpans[currentIndex];
+        $tagSpan.className = 'active';
+      }
+    } else {
+      if (currentIndex < $tagSpans.length) {
+        $tagSpan.classList.add('red-font');
+      }
+    }
+  });
+}
+typingTutor();
