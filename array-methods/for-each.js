f056ce62e54707029c6399a1e907e778735e996c
Frank Kang <holycow001@gmail.com>
2025-03-05 03:00:28 +0000

    finished to find

---
+++ array-methods/for-each.js
@@ -0,0 +1,8 @@
+"use strict";
+const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
+values.forEach((element) => {
+    console.log(element);
+});
+values.forEach((element, index) => {
+    console.log(values[values.length - 1 - index]);
+});
