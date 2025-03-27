71c4d18a1654dbd24283f8caa7eb9433c35ade3a
Frank Kang <holycow001@gmail.com>
2025-03-27 21:29:02 +0000

    in progress

---
+++ react-mutations/my-app/src/App.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars -- Remove me */
+import { useState } from 'react';
+import { Pokemon, PokemonList } from './PokemonList';
+
+const initialPokedex: Pokemon[] = [
+  { id: 1, name: 'Bulbasaur' },
+  { id: 4, name: 'Charmander' },
+  { id: 7, name: 'Squirtle' },
+  { id: 25, name: 'Pikachu' },
+  { id: 39, name: 'Jigglypuff' },
+];
+
+const addPokemon = { id: 40, name: 'Fakemon' };
+const updatePokemon = { id: 25, name: 'Peek-a-boo' };
+const removePokemon = { id: 4, name: 'Charmander' };
+
+export function App() {
+  const [pokedex, setPokedex] = useState(initialPokedex);
+
+  function handleAdd(toAdd: Pokemon): void {}
+
+  function handleUpdate(toUpdate: Pokemon): void {}
+
+  function handleRemove(toRemove: number): void {}
+
+  return (
+    <div>
+      <PokemonList pokedex={pokedex} />
+      <button onClick={() => handleAdd(addPokemon)}>Add</button>
+      <button onClick={() => handleUpdate(updatePokemon)}>Update</button>
+      <button onClick={() => handleRemove(removePokemon.id)}>Remove</button>
+    </div>
+  );
+}
