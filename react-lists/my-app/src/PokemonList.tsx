59ee3e472b6db4790db8cfc9cdf5e2225741037d
Frank Kang <holycow001@gmail.com>
2025-03-31 20:24:32 +0000

    lists

--- react-lists/my-app/src/PokemonList.tsx
+++ react-lists/my-app/src/PokemonList.tsx
@@ -1,20 +1,10 @@
-type Pokemon = {
-  number: string;
-  name: string;
-};
-
-export function PokemonList() {
-  const pokemons: Pokemon[] = [
-    { number: '001', name: 'Bulbasaur' },
-    { number: '004', name: 'Charmander' },
-    { number: '007', name: 'Squirtle' },
-    { number: '025', name: 'Pikachu' },
-    { number: '039', name: 'Jigglypuff' },
-  ];
-
-  const pokemonList = pokemons.map((pokemon) => (
-    <li key={pokemon.number}>{pokemon.name}</li>
-  ));
-
-  return <ul>{pokemonList}</ul>;
+export function PokemonList(props) {
+  const pokemons = props.pokedex;
+  return (
+    <ul>
+      {pokemons.map((pokemon) => (
+        <li key={pokemon.number}>{pokemon.name}</li>
+      ))}
+    </ul>
+  );
 }
