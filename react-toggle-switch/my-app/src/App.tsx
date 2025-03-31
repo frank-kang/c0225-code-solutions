00f4557e18e5f21c4d7c96051a2da7ea4a155fba
Frank Kang <holycow001@gmail.com>
2025-03-31 17:05:43 +0000

    working on taglel to switch

--- react-toggle-switch/my-app/src/App.tsx
+++ react-toggle-switch/my-app/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
 import { ToggleSwitch } from './ToggleSwitch';
 import './App.css';
 
 function App() {
-  const [status, setStatus] = useState(0);
-
   return <ToggleSwitch />;
 }
 
