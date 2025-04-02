f153e6f30898f26d4da4eeb8785443d0284a95bc
Frank Kang <holycow001@gmail.com>
2025-04-02 16:11:44 +0000

    react forms

--- react-forms/my-app/src/RegistrationFormControlled.tsx
+++ react-forms/my-app/src/RegistrationFormControlled.tsx
@@ -1,16 +1,33 @@
-import { FormEvent } from 'react';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 export function RegistrationFormControlled() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log({ username, password });
+  }
+
   return (
     <>
       <h2>Controlled Form</h2>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <label htmlFor="">
-          Username: <input type="text" />
+          Username:{' '}
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </label>
         <label htmlFor="">
-          Password: <input type="password" />
+          Password:{' '}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <label htmlFor="">
           <button>Submit</button>
--- react-forms/my-app/src/RegistrationFormUncontrolled.tsx
+++ react-forms/my-app/src/RegistrationFormUncontrolled.tsx
@@ -3,7 +3,9 @@ import { FormEvent } from 'react';
 export function RegistrationFormUncontrolled() {
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
+    const entries = new FormData(event.currentTarget);
+    const data = Object.fromEntries(entries);
+    console.log(data);
   }
 
   return (
@@ -11,10 +13,12 @@ export function RegistrationFormUncontrolled() {
       <h2>Uncontrolled Form</h2>
       <form onSubmit={handleSubmit}>
         <label>
-          User Name: <input name="myInput" />
+          User Name:{' '}
+          <input type="text" name="username" placeholder="username" />
         </label>
         <label htmlFor="">
-          Password: <input type="password" name="myPassword" />
+          Password:{' '}
+          <input type="password" name="password" placeholder="password" />
         </label>
         <label htmlFor="">
           <button>Submit</button>
