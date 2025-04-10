ed5be31480038ec1a2f8d21a12a4ac07b76523e4
Frank Kang <holycow001@gmail.com>
2025-04-10 15:58:26 +0000

    in progress

--- react-stopwatch/my-app/src/StopWatch.tsx
+++ react-stopwatch/my-app/src/StopWatch.tsx
@@ -3,13 +3,21 @@ import { FaPlay, FaPause } from 'react-icons/fa';
 
 export function StopWatch() {
   const [seconds, setSeconds] = useState();
+  const [elapsedSeconds, setElapsedSeonds] = useState(0);
+
+  function handlePlay() {
+    setInterval();
+  }
 
   return (
     <div className="stop-watch">
       <div className="watch-face">
-        <span className="counter">0</span>
+        <span className="counter">{seconds}</span>
       </div>
-      <FaPause size="2rem" className="start-stop" />
+      <div>
+        <FaPause onClick={handlePlay} size="2rem" />
+        <FaPlay size={'2rem'} />
+      </div>
     </div>
   );
 }
