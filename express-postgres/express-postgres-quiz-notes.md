d794c24a053123c8eddbd4d327c850b8e1283971
Frank Kang <holycow001@gmail.com>
2025-04-15 01:19:18 +0000

    express-postgres

--- express-postgres/express-postgres-quiz-notes.md
+++ express-postgres/express-postgres-quiz-notes.md
@@ -5,15 +5,15 @@
 Answer the following questions in the provided markdown file before turning in this exercise:
 
 - What is the purpose of the `pg` NPM package?
-
+  To add postgresql support to express.
 - How do you tell `pg` which database to connect to?
-
+  Create a variable and instantiate a new pg object, use the Pool method and pass it a connectionstring property set to the location of the db.
 - How do you send SQL to PostgreSQL from your Express server?
-
+  create a variable, pass it a query string to variable.query method.
 - How do you access the rows that get returned from the SQL query?
-
+  results.rows[0]
 - What must you always remember to put around your asynchronous route handlers? Why?
-
+  try catch
 - What is a SQL Injection Attack and how do you avoid it in `pg`?
 
 ## Notes
--- express-postgres/server.ts
+++ express-postgres/server.ts
@@ -24,6 +24,53 @@ app.get('/api/films', async (req, res, next) => {
   }
 });
 
+app.get('/api/films/:filmId', async (req, res, next) => {
+  try {
+    const { filmId } = req.params;
+    if (filmId === undefined) {
+      throw new ClientError(400, 'filmId is required');
+    }
+    const sql = `select "title"
+            from "films"
+            where "filmId" = $1;`;
+    const params = [filmId];
+    const result = await db.query(sql, params);
+    const film = result.rows[0];
+    if (!film) {
+      throw new ClientError(404, `film ${filmId} not found`);
+    }
+    res.send(film);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.put('/api/films/:filmId', async (req, res, next) => {
+  try {
+    const { filmId } = req.params;
+    const { title } = req.query;
+    if (filmId === undefined) {
+      throw new ClientError(400, 'filmId is required');
+    }
+    if (title === undefined) {
+      throw new ClientError(400, 'title is required');
+    }
+    const sql = `update "films"
+                set "title" = $1
+                where "filmId" = $2
+                returning * ;`;
+    const params = [title, filmId];
+    const result = await db.query(sql, params);
+    const film = result.rows[0];
+    if (!film) {
+      throw new ClientError(404, `film ${filmId} not found`);
+    }
+    res.send(film);
+  } catch (err) {
+    next(err);
+  }
+});
+
 app.use(errorMiddleware);
 
 app.listen(8080, () => {
