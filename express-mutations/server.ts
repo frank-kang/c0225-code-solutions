8d02f9aeb85d2b6bbd09998fd536b2ed5e9504b3
Frank Kang <holycow001@gmail.com>
2025-04-15 23:13:59 +0000

    express-mutations

--- express-mutations/server.ts
+++ express-mutations/server.ts
@@ -11,7 +11,7 @@ const db = new pg.Pool({
 });
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 
 app.post('/api/actors', async (req, res, next) => {
   try {
@@ -30,6 +30,57 @@ app.post('/api/actors', async (req, res, next) => {
     const params = [firstName, lastName];
     const result = await db.query(sql, params);
     res.status(201).json(result.rows[0]);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.put('/api/actors/:actorId', async (req, res, next) => {
+  try {
+    const { firstName, lastName } = req.body;
+    if (firstName === undefined) {
+      throw new ClientError(400, 'first name not provided');
+    }
+    if (lastName === undefined) {
+      throw new ClientError(400, 'last name not provided');
+    }
+    const { actorId } = req.params;
+    if (actorId === undefined) {
+      throw new ClientError(400, 'missing actorId');
+    }
+    if (!Number(actorId)) {
+      throw new ClientError(400, 'actorId needs to be a positive integer');
+    }
+
+    const sql = `update "actors"
+                set "firstName" = $1, "lastName"=$2
+                where "actorId"=$3
+                returning *;`;
+    const params = [firstName, lastName, actorId];
+    const result = await db.query(sql, params);
+    if (!result.rows[0]) {
+      throw new ClientError(404, `actorId: ${actorId} not found`);
+    }
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.delete('/api/actors/:actorId', async (req, res, next) => {
+  try {
+    const { actorId } = req.params;
+    if (actorId === undefined) {
+      throw new ClientError(400, 'missing actorId');
+    }
+    if (!Number(actorId)) {
+      throw new ClientError(400, 'actorId needs to be a positive integer');
+    }
+    const sql = `delete from "actors"
+                where "actorId" = $1`;
+    const params = [actorId];
+    const result = await db.query(sql, params);
+    res.status(204).json(result.rows[0]);
   } catch (err) {
     next(err);
   }
